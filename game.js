a1451d96bfbb959a27d02cffb011f291d04db75a
Devansh Biyani <devanshbiyani@gmail.com>
2023-02-16 10:10:40 +0530

    final

--- game.js
+++ game.js
@@ -1,129 +1,46 @@
-// Define canvas and context
-const canvas = document.getElementById("maze-canvas");
-const ctx = canvas.getContext("2d");
+const player = document.querySelector('.player');
+const playerImage = document.querySelector('#player-image');
+const maze = document.querySelector('.maze');
+const squares = document.querySelectorAll('.square');
+let isGameOver = false;
 
-// Define maze properties
-const mazeWidth = 600;
-const mazeHeight = 400;
-const cellSize = 40;
+playerImage.addEventListener('change', () => {
+  player.style.backgroundImage = `url(${playerImage.value})`;
+});
 
-// Define player properties
-const playerWidth = 20;
-const playerHeight = 20;
-let playerX = 0;
-let playerY = 0;
+document.addEventListener('keydown', (event) => {
+  if (isGameOver) return;
 
-// Define start button and instructions
-const startBtn = document.getElementById("start-btn");
-const instructions = document.getElementById("instructions");
+  const key = event.key;
+  const playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue('left'));
 
-// Define maze and player objects
-let maze = {};
-let player = {};
+  if (key === 'ArrowRight' && playerLeft < 250) {
+    player.style.left = `${playerLeft + 50}px`;
+  } else if (key === 'ArrowLeft' && playerLeft > 0) {
+    player.style.left = `${playerLeft - 50}px`;
+  }
 
-// Start the game when the button is clicked
-startBtn.addEventListener("click", startGame);
-
-function startGame() {
-  // Remove the start button and instructions
-  startBtn.style.display = "none";
-  instructions.style.display = "none";
-
-  // Generate the first level of the maze
-  const level = 1;
-  generateMaze(level);
-
-  // Move the player when the arrow keys are pressed
-  document.addEventListener("keydown", movePlayer);
-}
-
-function generateMaze(level) {
-  // Define the maze generation algorithm here
-  // This function should create a 2D array that represents the maze
-  // Each cell in the maze should be either a wall or a path
-  // The maze should have a start point and an end point
-  // The necessary documents should be placed at specific locations in the maze
-  // The player should start at the start point
-
-  // Draw the maze on the canvas
-  for (let x = 0; x < mazeWidth; x += cellSize) {
-    for (let y = 0; y < mazeHeight; y += cellSize) {
-      if (maze[x / cellSize][y / cellSize] === "wall") {
-        ctx.fillStyle = "black";
-      } else {
-        ctx.fillStyle = "white";
-      }
-      ctx.fillRect(x, y, cellSize, cellSize);
+  // Collision detection
+  const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+  const playerLeftOffset = player.offsetLeft;
+  const playerRightOffset = player.offsetLeft + player.offsetWidth;
+  const playerBottomOffset = player.offsetTop + player.offsetHeight;
+  
+  squares.forEach((square) => {
+    const squareTop = square.offsetTop;
+    const squareLeftOffset = square.offsetLeft;
+    const squareRightOffset = square.offsetLeft + square.offsetWidth;
+    const squareBottomOffset = square.offsetTop + square.offsetHeight;
+    
+    if (
+      playerTop <= squareBottomOffset &&
+      playerBottomOffset >= squareTop &&
+      playerRightOffset >= squareLeftOffset &&
+      playerLeftOffset <= squareRightOffset
+    ) {
+      // Collision detected
+      isGameOver = true;
+      alert('Game over!');
     }
-  }
-
-  // Draw the player at the start point
-  player = {
-    x: start.x * cellSize + cellSize / 2 - playerWidth / 2,
-    y: start.y * cellSize + cellSize / 2 - playerHeight / 2,
-  };
-  ctx.fillStyle = "red";
-  ctx.fillRect(player.x, player.y, playerWidth, playerHeight);
-    // Set the canvas dimensions
-    canvas.width = mazeWidth;
-    canvas.height = mazeHeight;
-  
-    // Generate the maze and set the player and end positions
-    maze = generateRandomMaze(mazeWidth / cellSize, mazeHeight / cellSize);
-    player.x = start.x * cellSize;
-    player.y = start.y * cellSize;
-    end.x = Math.floor(Math.random() * mazeWidth / cellSize) * cellSize;
-    end.y = Math.floor(Math.random() * mazeHeight / cellSize) * cellSize;
-  
-    // Start the game loop
-    gameLoop();
-  }
-  
-
-
-function movePlayer(e) {
-  switch (e.keyCode) {
-    case 37: // left arrow
-      if (player.x > 0 && maze[(player.x - 1) / cellSize][player.y / cellSize] !== "wall") {
-        player.x -= cellSize;
-      }
-      break;
-    case 38: // up arrow
-      if (player.y > 0 && maze[player.x / cellSize][(player.y - 1) / cellSize] !== "wall") {
-        player.y -= cellSize;
-      }
-      break;
-    case 39: // right arrow
-      if (player.x < mazeWidth - cellSize && maze[(player.x + 1) / cellSize][player.y / cellSize] !== "wall") {
-        player.x += cellSize;
-      }
-      break;
-    case 40: // down arrow
-      if (player.y < mazeHeight - cellSize && maze[player.x / cellSize][(player.y + 1) / cellSize] !== "wall") {
-        player.y += cellSize;
-      }
-      break;
-  }
-
-  // Clear the canvas and redraw the maze and player
-    ctx.clearRect(0, 0, mazeWidth, mazeHeight);
-for (let x = 0; x < mazeWidth; x += cellSize) {
-for (let y = 0; y < mazeHeight; y += cellSize) {
-if (maze[x / cellSize][y / cellSize] === "wall") {
-ctx.fillStyle = "black";
-} else {
-ctx.fillStyle = "white";
-}
-ctx.fillRect(x, y, cellSize, cellSize);
-}
-}
-ctx.fillStyle = "red";
-ctx.fillRect(player.x, player.y, playerWidth, playerHeight);
-
-// Check if the player has reached the end of the maze
-if (player.x / cellSize === end.x && player.y / cellSize === end.y) {
-// If so, generate the next level
-const nextLevel = level + 1;
-generateMaze(nextLevel);
-}
-}
+  });
+});
